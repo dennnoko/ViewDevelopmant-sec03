eec9164f3c4b68d4ed641e502df2dee8770bcfb7
でんのこ <dennnoko314@gmail.com>
2023-08-10 01:11:15 +0900

    Add a new function. If btn is clicked, a message is displayed.

--- app/src/main/java/com/example/viewdevelopmant_sec03/MainActivity.kt
+++ app/src/main/java/com/example/viewdevelopmant_sec03/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.viewdevelopmant_sec03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //If clicked btn ,display message
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val txtName = findViewById<EditText>(R.id.txtName)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        btnSend.setOnClickListener {
+            txtResult.text = "Hello,${txtName.text}!"
+        }
     }
 }
