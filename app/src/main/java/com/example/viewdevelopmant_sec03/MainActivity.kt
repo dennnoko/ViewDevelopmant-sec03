bab048c6ea85e0930c808d83a8604d7ab8e688cf
でんのこ <dennnoko314@gmail.com>
2023-08-10 15:56:51 +0900

    Add SeekBar

--- app/src/main/java/com/example/viewdevelopmant_sec03/MainActivity.kt
+++ app/src/main/java/com/example/viewdevelopmant_sec03/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.viewdevelopmant_sec03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //get Widget
+        val seekBar1 = findViewById<SeekBar>(R.id.seekBar1)
+
+        //Listener
+        seekBar1.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Toast.makeText(
+                    this@MainActivity,
+                    String.format(Locale.JAPAN, "現在地:%d", progress),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
